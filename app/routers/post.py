13082aeae9ab29eee4563941e68c4fff6c8c22c9
Ilqar Rasulov <ilgarrasulow@yandex.ru>
2022-04-24 15:51:48 +0200

    test commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,7 +14,7 @@ router=APIRouter(
 def get_posts(db: Session=Depends(get_db), current_user: int = Depends(oath2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts=cursor.fetchall()
-    posts=db.query(models.Post).all()
+    posts=db.query(models.Post).filter(models.Post.id==current_user.id).all()
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
