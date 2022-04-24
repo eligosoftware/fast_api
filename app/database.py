24c9f31f250e887dcae0c039971d7560317cedec
Ilqar Rasulov <ilgarrasulow@yandex.ru>
2022-04-24 18:02:38 +0200

    query parameters and main.py cleanup

--- app/database.py
+++ app/database.py
@@ -3,6 +3,9 @@ from multiprocessing.spawn import import_main_path
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 #SQL_ALCHEMY_DATABASE='postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
 SQL_ALCHEMY_DATABASE='postgresql://postgres:root@localhost/fastapi'
@@ -19,3 +22,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+#     try:
+#         conn=psycopg2.connect(host='localhost',database='fastapi',user='postgres',
+#         password='root',cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Successfully connected to database")
+#         break
+#     except Exception as error:
+        
+#         print("Connection to database has failed :(. Error: ",error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import  FastAPI
-import time
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from . import models
 from .database import engine
 from .routers import post,user,auth
@@ -10,28 +7,6 @@ from .routers import post,user,auth
 models.Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
-
-
-
-while True:
-    try:
-        conn=psycopg2.connect(host='localhost',database='fastapi',user='postgres',
-        password='root',cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Successfully connected to database")
-        break
-    except Exception as error:
-        
-        print("Connection to database has failed :(. Error: ",error)
-        time.sleep(2)
-
-
-
-my_posts=[{"title":"title of post 1","content":"content of post 1","id":1},
-        {"title":"title of post 2   ","content":"content of post 2","id":2}
-        ]
-
-
 
 app.include_router(post.router)
 app.include_router(user.router)
